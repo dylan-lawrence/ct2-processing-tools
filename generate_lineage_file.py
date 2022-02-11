6a4cae7d073bd0a56bb5e4930393482723df8186
Dylan Lawrence <dylan.lawrence@wustl.edu>
2022-02-11 16:59:21 -0600

    Update generate_lineage_file.py
    fixed some minor header stuff

--- generate_lineage_file.py
+++ generate_lineage_file.py
@@ -12,18 +12,18 @@ parser.add_argument("--output-file", metavar="o", type=str, nargs=1, help="filen
 args=parser.parse_args()
 #args.directory args.output_file
 
-gbf_extraction_pattern = re.compile("-- closest relative:.*gi\|")
+fsa_extraction_pattern = re.compile("-- closest relative:.*gi\|")
 
 with open(args.output_file[0], 'w') as f:
-	f.write("Domain\tRealm\tKingdom\tPhylum\tClass\tOrder\tFamily\tSubfamily\tGenus\tSpecies\n")
+	f.write("CENOTE_NAME\tDomain\tRealm\tKingdom\tPhylum\tClass\tOrder\tFamily\tSubfamily\tGenus\tSpecies\n")
 	for directory in os.listdir(args.directory[0]):
 	#open up the summary file for this
 		summary_data=[line.strip('\n') for line in open(args.directory[0] + directory + "/" + directory + "_CONTIG_SUMMARY.tsv")][1::]
 		for entry in summary_data:
 			contig=entry.split('\t')[1]
 			try:
-				gbf_file=(Path(args.directory[0] + directory + "/sequin_and_genome_maps/" + contig + ".fsa").read_text()).split('\n')[0]
-				lineage = gbf_extraction_pattern.search(gbf_file).group(0).lstrip("-- closest relative: ").rstrip(" gi|").split("; ")
+				fsa_file=(Path(args.directory[0] + directory + "/sequin_and_genome_maps/" + contig + ".fsa").read_text()).split('\n')[0]
+				lineage = fsa_extraction_pattern.search(fsa_file).group(0).lstrip("-- closest relative: ").rstrip(" gi|").split("; ")
 				f.write(contig + "\t" + "\t".join(lineage) + '\n')
 			except:
 				print(contig + " Did not have a corresponding .gbf file")
